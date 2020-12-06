a113eb54196049d97ebf773664691e206decb3a4
Milan Radeta <milan.radeta.peca@gmail.com>
2020-12-06 03:49:00 +0100

    Add calculate_portion_saved

--- 6.0001/ps1/calculate_savings.py
+++ 6.0001/ps1/calculate_savings.py
@@ -40,3 +40,40 @@ def calculate_savings(annual_salary, portion_saved, total_cost, semi_annual_rais
             months_needed, semi_annual_raise, annual_salary)
 
     return months_needed
+
+
+def calculate_portion_saved(annual_salary, total_cost, semi_annual_raise=0, months=12, r=0.04, portion_down_payment=0.25, end=10**4, toleration=100):
+    start = 0
+    denom = end
+    start_annual_salary = annual_salary
+    down_payment = total_cost * portion_down_payment
+    portion_saved = None
+    bisection_steps = 0
+
+    while start < end - 1:
+        bisection_steps += 1
+        mid = (start + end) // 2
+        portion_saved = mid * 1. / denom
+        annual_salary = start_annual_salary
+        monthly_salary = annual_salary / months_per_year
+        current_savings = 0
+        months_needed = months
+
+        while months_needed > 0:
+            months_needed -= 1
+            current_savings = add_savings(
+                current_savings, r, monthly_salary, portion_saved)
+            annual_salary, monthly_salary = update_annual_salary(
+                months_needed, semi_annual_raise, annual_salary)
+            if current_savings > down_payment - toleration:
+                break
+
+        if abs(current_savings - down_payment) < toleration:
+            break
+        elif current_savings > down_payment + toleration:
+            end = mid
+        elif current_savings < down_payment - toleration:
+            portion_saved = None
+            start = mid
+
+    return portion_saved, bisection_steps
