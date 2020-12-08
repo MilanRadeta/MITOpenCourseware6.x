01e4d59c56819dc96ad772da4b095e5680ce9783
Milan Radeta <milan.radeta.peca@gmail.com>
2020-12-08 21:37:48 +0100

    Implement exercise 3

--- 6.s095/02/partysmart2.py
+++ 6.s095/02/partysmart2.py
@@ -11,7 +11,20 @@ sched2 = [(6.0, 8.0), (6.5, 12.0), (6.5, 7.0), (7.0, 8.0), (7.5, 10.0), (8.0, 9.
           (8.0, 10.0), (9.0, 12.0), (9.5, 10.0), (10.0, 11.0), (10.0, 12.0), (11.0, 12.0)]
 sched3 = [(6, 7), (7, 9), (10, 11), (10, 12), (8, 10), (9, 11), (6, 8),
           (9, 10), (11, 12), (11, 13), (11, 14)]
-
+sched4 = [
+    (6.0, 8.0, 2),
+    (6.5, 12.0, 1),
+    (6.5, 7.0, 2),
+    (7.0, 8.0, 2),
+    (7.5, 10.0, 3),
+    (8.0, 9.0, 2),
+    (8.0, 10.0, 1),
+    (9.0, 12.0, 2),
+    (9.5, 10.0, 4),
+    (10.0, 11.0, 2),
+    (10.0, 12.0, 3),
+    (11.0, 12.0, 7),
+]
 
 def bestTimeToPartySmart(schedule, ystart = 0, yend = 24):
     # Convert schedule to list of start times and end times marked as such
@@ -33,23 +46,27 @@ def bestTimeToPartySmart(schedule, ystart = 0, yend = 24):
 def bestTimeToPartyIntersection(schedule):
     # Convert schedule to list of start times and end times marked as such
     best_count = 0
+    best_celebrities_count = 0
     best_time = None
     for t in schedule:
         if t == schedule[0]:
             continue
 
-        count = 1
+        count = t[2] if len(t) > 2 else 1
+        celebrities_count = 1
         for t2 in schedule:
             if t != t2 and t2[0] <= t[0] < t2[1]:
-                count += 1
+                count += t2[2] if len(t2) > 2 else 1
+                celebrities_count += 1
         
         if count > best_count:
             best_time = t[0]
             best_count = count
+            best_celebrities_count = celebrities_count
 
     # Output best time to party
     print('Best time to attend the party is at', best_time,
-          'o\'clock', ':', best_count, 'celebrities will be attending!')
+          'o\'clock', ':', best_celebrities_count, 'celebrities will be attending with score of', best_count,'!')
 
 
 # Sort the elements of tlist in ascending order
@@ -91,3 +108,4 @@ bestTimeToPartySmart(sched3, 9, 10)
 bestTimeToPartyIntersection(sched)
 bestTimeToPartyIntersection(sched2)
 bestTimeToPartyIntersection(sched3)
+bestTimeToPartyIntersection(sched4)
