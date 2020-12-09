b576cf3dc1ccebf29c99cb58d5dd55041b56ed30
Milan Radeta <milan.radeta.peca@gmail.com>
2020-12-09 18:31:57 +0100

    Add pretty print Minor refactor

--- 6.s095/07/tiling.py
+++ 6.s095/07/tiling.py
@@ -5,9 +5,11 @@
 #This version works directly on the given grid, and does NOT make copies
 #of the grid for recursive calls.
 
+import string
+
 EMPTYPIECE = -1
 
-
+diffs = [(0,0), (0,1), (1,0), (1,1)]
 
 #This procedure is the main engine of recursive algorithm
 #nextPiece is the number of next available tromino piece
@@ -17,10 +19,10 @@ def recursiveTile(yard, size, originR, originC, rMiss, cMiss, nextPiece):
     #quadrant of missing square: 0 (upper left), 1 (upper right),
     #                            2 (lower left), 3 (lower right)
     quadMiss = 2*(rMiss >= size//2) + (cMiss >= size//2)
-    
+
     #base case of 2x2 yard
     if size == 2: 
-        piecePos = [(0,0), (0,1), (1,0), (1,1)]
+        piecePos = diffs.copy()
         piecePos.pop(quadMiss)
         for (r, c) in piecePos:
             yard[originR + r][originC + c] = nextPiece
@@ -28,29 +30,29 @@ def recursiveTile(yard, size, originR, originC, rMiss, cMiss, nextPiece):
         return nextPiece
 
     #recurse on each quadrant
-    
+    half_size = size // 2    
     for quad in range(4):
         #Each quadrant has a different origin
         #Quadrant 0 has origin (0, 0), Quadrant 1 has origin (0, size//2)
         #Quadrant 2 has origin (size//2, 0), Quadrant 3 has origin (size//2, size//2)
-        shiftR = size//2 * (quad >= 2)
-        shiftC = size//2 * (quad % 2 == 1)
+        shiftR = half_size * (quad >= 2)
+        shiftC = half_size * (quad % 2 == 1)
         if quad == quadMiss:
             #Pass the new origin and the shifted rMiss and cMiss
-            nextPiece = recursiveTile(yard, size//2, originR + shiftR,\
+            nextPiece = recursiveTile(yard, half_size, originR + shiftR,\
                 originC + shiftC, rMiss - shiftR, cMiss - shiftC, nextPiece)
 
         else:
             #The missing square is different for each of the other 3 quadrants
-            newrMiss = (size//2 - 1) * (quad < 2)
-            newcMiss = (size//2 - 1) * (quad % 2 == 0)
-            nextPiece = recursiveTile(yard, size//2, originR + shiftR,\
+            newrMiss = (half_size - 1) * (quad < 2)
+            newcMiss = (half_size - 1) * (quad % 2 == 0)
+            nextPiece = recursiveTile(yard, half_size, originR + shiftR,\
                             originC + shiftC, newrMiss, newcMiss, nextPiece)
 
 
     #place center tromino
-    centerPos = [(r + size//2 - 1, c + size//2 - 1)
-                 for (r,c) in [(0,0), (0,1), (1,0), (1,1)]]
+    centerPos = [(r + half_size - 1, c + half_size - 1)
+                 for (r,c) in diffs]
     centerPos.pop(quadMiss)
     for (r,c) in centerPos: # assign tile to 3 center squares
         yard[originR + r][originC + c] = nextPiece
@@ -66,16 +68,26 @@ def tileMissingYard(n, rMiss, cMiss):
     recursiveTile(yard, 2**n, 0, 0, rMiss, cMiss, 0)
     return yard
 
+ARROWS = ['←','↗','↓','↖','→','↙','↑','↘']
+LETTERS = string.ascii_letters
+UPPERS = string.ascii_uppercase
+LOWERS = string.ascii_lowercase
+XYZ = 'UWXYZ'
+OPS = '!@#%^&*()_+-=[]:|<>"''\\/'
+OPS2 = '+-[]<>|-"\\/'
+SPECS = '|-\\/<>^v'
+
 #This procedure prints a given tiled yard using letters for tiles
-def printYard(yard):
+def printYard(yard, signs=SPECS, repeat=2, end=' '):
+    size = len(signs)
     for i in range(len(yard)):
-        row = ''
         for j in range(len(yard[0])):
             if yard[i][j] != EMPTYPIECE:
-                row += chr((yard[i][j] % 26) + ord('A'))
+                print(('%s' % signs[yard[i][j] % size]) * repeat, end=end)
             else:
-                row += ' '
-        print (row)
+                print('?' * repeat, end=end)
+        print()
+    print()
 
 
 printYard(tileMissingYard(3, 4, 6))
