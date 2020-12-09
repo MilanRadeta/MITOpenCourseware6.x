c38027d5be4b7872dd6a4d229dab81de9afb8138
Milan Radeta <milan.radeta.peca@gmail.com>
2020-12-09 12:52:30 +0100

    Optimize 4 queens

--- 6.s095/05/4queens-matrix.py
+++ 6.s095/05/4queens-matrix.py
@@ -26,19 +26,20 @@ def noConflicts(board, current, qindex, n = 4):
     #Check the two diagonals from the current queen
     #The first loop is for the diagonal /
     #The second loop is for the diagonal \
-    k = 1
-    while qindex - k >= 0 and current - k >= 0:
-        if board[qindex - k][current - k] == 1:
-            return False
-        k += 1
-    k = 1
-    while qindex + k < n and current - k >= 0:
-        if board[qindex + k][current - k] == 1:
-            return False
-        k += 1
+    ks = [1, -1]
+    for k in ks:
+        while 0 <= qindex + k < n and current - abs(k) >= 0:
+            if board[qindex + k][current - abs(k)] == 1:
+                return False
+            k += k
 
     return True 
 
+
+def printBoard(board):
+    for line in board:
+        print(line)
+    print()
 
 #This procedure places 4 Queens on a board so they don't conflict
 #It assumes n = 4 and won't work with other n!
@@ -61,7 +62,7 @@ def FourQueens(n=4):
                         for m in range(n):
                             board[m][3] = 1
                             if noConflicts(board, 3, m, n):
-                                print (board)
+                                printBoard (board)
                             board[m][3] = 0
                     board[k][2] = 0
             board[j][1] = 0
