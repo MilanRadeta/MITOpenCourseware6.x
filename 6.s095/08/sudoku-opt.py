5c89d72e2ce9e0f75e9e76662a0ddc8d510891a9
Milan Radeta <milan.radeta.peca@gmail.com>
2020-12-10 00:31:27 +0100

    Make implications without specifying first field Return easiest to fill cell from make implications Support making init implications before guessing Support finding next cell by easiest implications

--- 6.s095/08/sudoku-opt.py
+++ 6.s095/08/sudoku-opt.py
@@ -35,11 +35,15 @@ def findUsedValues(grid, coords):
 
 #This procedure makes implications based on existing numbers on squares
 def makeImplications(grid, i, j, e):
-    grid[i][j] = e
-    impl = [(i, j, e)]
+    impl = []
+    if i >= 0 and j >= 0:
+        grid[i][j] = e
+        impl.append((i, j, e))
 
+    easiest_cell = (-1, -1, None)
     made_changes = True
     while made_changes:
+        easiest_cell = (-1, -1, None)
         made_changes = False
         for sector in sectors:
 
@@ -51,6 +55,7 @@ def makeImplications(grid, i, j, e):
                 coords.extend([(x,sin[1]) for x in range(sudoku_size)])
                 usedVals = findUsedValues(grid, coords)
                 left = sin[2].difference(usedVals)
+                sin[2] = left
                             
                 #check if the vset is a singleton
                 if len(left) == 1:
@@ -59,8 +64,11 @@ def makeImplications(grid, i, j, e):
                     if isValid(grid, sin[0], sin[1], val):
                         grid[sin[0]][sin[1]] = val
                         impl.append((sin[0], sin[1], val))
+                elif easiest_cell[2] is None or len(easiest_cell[2]) > len(left):
+                    easiest_cell = sin
+
                 
-    return impl
+    return impl, easiest_cell
 
 #This procedure undoes all the implications
 def undoImplications(grid, impls):
@@ -72,9 +80,20 @@ def undoImplications(grid, impls):
 #This procedure fills in the missing squares of a Sudoku puzzle
 #obeying the Sudoku rules by guessing when it has to and performing
 #implications when it can
-def solveSudoku(grid, i=0, j=0, backtracks=0):
+def solveSudoku(grid, makeInitImplications=False, findNextCellByImpl=False, i=0, j=0, backtracks=0):
+    initimpl = None
+    next_cell = None
+    if findNextCellByImpl:
+        makeInitImplications = True
+
+    if makeInitImplications:
+        initimpl, next_cell = makeImplications(grid, -1, -1, 0)
+
     #find the next empty cell to fill
-    i, j = findNextCellToFill(grid)
+    if findNextCellByImpl:
+        i, j, vals = next_cell
+    else:
+        i, j = findNextCellToFill(grid)
     if i == -1:
         return True, backtracks
 
@@ -83,19 +102,21 @@ def solveSudoku(grid, i=0, j=0, backtracks=0):
         #Try different values in i, j location
         if isValid(grid, i, j, e):
 
-            impl = makeImplications(grid, i, j, e)
+            impl, sin = makeImplications(grid, i, j, e)
             
-            solved, backtracks = solveSudoku(grid, i, j, backtracks)
+            solved, backtracks = solveSudoku(grid, makeInitImplications, findNextCellByImpl, i, j, backtracks)
             if solved:
                 return True, backtracks
             #Undo the current cell for backtracking
             backtracks += 1
             undoImplications(grid, impl)
 
+    if makeInitImplications:
+        undoImplications(grid, initimpl)
     return False, backtracks
 
 for inp in all_inputs:
-    solved, backtracks = solveSudoku(inp)
+    solved, backtracks = solveSudoku(inp, makeInitImplications=True, findNextCellByImpl=True)
     printSudoku(inp)
     print ('Backtracks = ', backtracks)
     print('-'*30)
