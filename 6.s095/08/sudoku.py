3d43287e9e4dbb396be851a6752f6610a557511b
Milan Radeta <milan.radeta.peca@gmail.com>
2020-12-09 23:15:00 +0100

    Optimize sudoku solver

--- 6.s095/08/sudoku.py
+++ 6.s095/08/sudoku.py
@@ -3,19 +3,28 @@
 #Given a partially filled in Sudoku board, complete the puzzle
 #obeying the rules of Sudoku
 
-#Global variable set to 0
-backtracks = 0
-
+sudoku_size = 9
+segment_size = sudoku_size // 3
 
 #This procedure finds the next empty square to fill on the Sudoku grid
 def findNextCellToFill(grid):
     #Look for an unfilled grid location
-    for x in range(0, 9):
-        for y in range(0, 9):
+    for x in range(sudoku_size):
+        for y in range(sudoku_size):
             if grid[x][y] == 0:
                 return x, y
     return -1, -1
 
+
+def checkSegment(grid, i, j, e):
+    #finding the top left x,y co-ordinates of
+    #the section or sub-grid containing the i,j cell
+    secTopX, secTopY = segment_size *(i//segment_size), segment_size *(j//segment_size)
+    for x in range(secTopX, secTopX+segment_size):
+        for y in range(secTopY, secTopY+segment_size):
+            if grid[x][y] == e:
+                return False
+    return True
 
 #This procedure checks if setting the (i, j) square to e is valid
 def isValid(grid, i, j, e):
@@ -23,47 +32,41 @@ def isValid(grid, i, j, e):
     if rowOk:
         columnOk = all([e != grid[x][j] for x in range(9)])
         if columnOk:
-            #finding the top left x,y co-ordinates of
-            #the section or sub-grid containing the i,j cell
-            secTopX, secTopY = 3 *(i//3), 3 *(j//3)
-            for x in range(secTopX, secTopX+3):
-                for y in range(secTopY, secTopY+3):
-                    if grid[x][y] == e:
-                        return False
-            return True
+            return checkSegment(grid, i, j, e)
     return False
 
 #This procedure fills in the missing squares of a Sudoku puzzle
 #obeying the Sudoku rules through brute-force guessing and checking
-def solveSudoku(grid, i=0, j=0):
-
-    global backtracks
-
+def solveSudoku(grid, i=0, j=0, backtracks=0):
     #find the next cell to fill
     i, j = findNextCellToFill(grid)
     if i == -1:
-        return True
+        return True, backtracks
 
-    for e in range(1, 10):
+    for e in range(sudoku_size):
+        e += 1
         #Try different values in i, j location
         if isValid(grid, i, j, e):
             grid[i][j] = e
-            if solveSudoku(grid, i, j):
-                return True
+            solved, backtracks = solveSudoku(grid, i, j, backtracks)
+            if solved:
+                return True, backtracks
             
             #Undo the current cell for backtracking
             backtracks += 1
             grid[i][j] = 0
 
-    return False
+    return False, backtracks
 
 
 def printSudoku(grid):
     numrow = 0
     for row in grid:
-        if numrow % 3 == 0 and numrow != 0:
+        if numrow % segment_size == 0 and numrow != 0:
             print (' ')
-        print (row[0:3], ' ', row[3:6], ' ', row[6:9])
+        for i in range(0, sudoku_size, segment_size):
+            print (row[i:i+segment_size], end='  ')
+        print()
         numrow += 1       
     return
 
@@ -118,25 +121,9 @@ diff  = [[0,0,5,3,0,0,0,0,0],
          [0,0,0,0,0,9,7,0,0]]
 
 
-
-print(solveSudoku(input))
-printSudoku(input)
-print ('Backtracks = ', backtracks)
-
-backtracks = 0
-print(solveSudoku(inp2))
-printSudoku(inp2)
-print ('Backtracks = ', backtracks)
-
-backtracks = 0
-printSudoku(hard)
-print(solveSudoku(hard))
-printSudoku(hard)
-print ('Backtracks = ', backtracks)
-
-backtracks = 0
-printSudoku(diff)
-print(solveSudoku(diff))
-printSudoku(hard)
-print ('Backtracks = ', backtracks)
-
+inps = [input, inp2, hard, diff]
+for inp in inps:
+    solved, backtracks = solveSudoku(inp)
+    printSudoku(inp)
+    print ('Backtracks = ', backtracks)
+    print('-'*30)
