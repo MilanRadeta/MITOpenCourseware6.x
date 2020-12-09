b317e1d8d2055871c75f9d765304bf8a31f3d5cd
Milan Radeta <milan.radeta.peca@gmail.com>
2020-12-09 15:32:38 +0100

    Add predefined locations

--- 6.s095/06/nqueens.py
+++ 6.s095/06/nqueens.py
@@ -5,9 +5,9 @@
 
 #This procedure initializes the board to be empty, calls the recursive N-queens
 #procedure and prints the returned solution
-def nQueens(size):
+def nQueens(size, locations = None):
     board = [-1] * size
-    rQueens(board, 0, size)
+    rQueens(board, 0, size, locations)
     printBoard (board)
 
 #This procedure checks that the most recently placed queen on column current
@@ -24,17 +24,22 @@ def noConflicts(board, current):
 #This procedure places a queens on the board on a given column so it does
 #not conflict with the existing queens, and then calls itself recursively
 #to place subsequent queens till the requisite number of queens are placed
-def rQueens(board, current, size):
+def rQueens(board, current, size, locations):
     if (current == size):
         return True
     else:
-        for i in range(size):
+        arr = range(size)
+
+        if locations is not None and locations[current] >= 0:
+            arr = [locations[current]]
+            
+        for i in arr:
             board[current] = i
             if (noConflicts(board, current)):
-                done = rQueens(board, current + 1, size)
+                done = rQueens(board, current + 1, size, locations)
                 if (done):
                     return True
-        return False
+    return False
   
 
 def printBoard(board):
@@ -45,4 +50,5 @@ def printBoard(board):
         print()
     print()
 
-nQueens(20)
+# nQueens(20)
+nQueens(10, [-1, -1, 4, -1, -1, -1, -1, 0, -1, 5])
