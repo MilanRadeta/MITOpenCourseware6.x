82639449b5e2c1c2d399f456aab0381d091293d8
Milan Radeta <milan.radeta.peca@gmail.com>
2021-01-02 02:39:07 +0100

    Implement BinOp and subclasses

--- 6.009/09-classes,-inheritance,-oop/lab8/lab.py
+++ 6.009/09-classes,-inheritance,-oop/lab8/lab.py
@@ -38,6 +38,51 @@ class Num(Symbol):
     def __repr__(self):
         return 'Num(' + repr(self.n) + ')'
 
+class BinOp(Symbol):
+    def __init__(self, operator, left, right):
+        """
+        Initializer.  Store an instance variable called `operator`, containing the
+        binary operator symbol, and `left` and `right` operands.
+        """
+        self.operator = operator
+        self.left = BinOp.process_operator(left)
+        self.right = BinOp.process_operator(right)
+
+    @staticmethod
+    def process_operator(operator):
+        """
+        Processes operator to adequate Symbol
+        """
+        if type(operator) is str:
+            return Var(operator)
+        if type(operator) in (float, int):
+            return Num(operator)
+        if not isinstance(operator, Symbol):
+            raise TypeError
+
+    def __str__(self):
+        return '%s %s %s' % (self.left, self.operator, self.right)
+
+    def __repr__(self):
+        return 'BinOp(%s, %s, %s)' % (self.operator, repr(self.left), repr(self.right))
+
+class Add(BinOp):
+    def __init__(self, left, right):
+        super().__init__('+', left, right)
+
+class Sub(BinOp):
+    def __init__(self, left, right):
+        super().__init__('-', left, right)
+
+class Mul(BinOp):
+    def __init__(self, left, right):
+        super().__init__('*', left, right)
+
+class Div(BinOp):
+    def __init__(self, left, right):
+        super().__init__('/', left, right)
+    
+
 
 if __name__ == '__main__':
     doctest.testmod()
