ae2c774c74363556fbc391cc812ab6c79f0c1bb3
Milan Radeta <milan.radeta.peca@gmail.com>
2021-01-02 02:56:37 +0100

    Implement parenthesization

--- 6.009/09-classes,-inheritance,-oop/lab8/lab.py
+++ 6.009/09-classes,-inheritance,-oop/lab8/lab.py
@@ -39,40 +39,55 @@ class Num(Symbol):
         return 'Num(' + repr(self.n) + ')'
 
 class BinOp(Symbol):
-    def __init__(self, operator, left, right):
+    def __init__(self, operator, left, right, precedence=1):
         """
         Initializer.  Store an instance variable called `operator`, containing the
         binary operator symbol, and `left` and `right` operands.
         """
         self.operator = operator
-        self.left = BinOp.process_operator(left)
-        self.right = BinOp.process_operator(right)
+        self.precedence = precedence
+        self.left = BinOp.process_operands(left)
+        self.right = BinOp.process_operands(right)
 
     @staticmethod
-    def process_operator(operator):
+    def process_operands(operand):
         """
         Processes operator to adequate Symbol
         """
-        if type(operator) is str:
-            return Var(operator)
-        if type(operator) in (float, int):
-            return Num(operator)
-        if not isinstance(operator, Symbol):
+        if type(operand) is str:
+            return Var(operand)
+        if type(operand) in (float, int):
+            return Num(operand)
+        if not isinstance(operand, Symbol):
             raise TypeError
 
+    def parenthesize_operands(self, operand, check_same_precedence=False):
+        """
+        Parenthesize operands if needed by checking their precedence
+        """
+        if isinstance(operand, BinOp):
+            if operand.precedence < self.precedence:
+                return '(%s)' % operand
+            if check_same_precedence and type(self) in (Div, Sub) and operand.precedence == self.precedence:
+                return '(%s)' % operand
+        return operand
+        
+
     def __str__(self):
-        return '%s %s %s' % (self.left, self.operator, self.right)
+        left = self.parenthesize_operands(self.left)
+        right = self.parenthesize_operands(self.right, True)
+        return '%s %s %s' % (left, self.operator, right)
 
     def __repr__(self):
         return 'BinOp(%s, %s, %s)' % (self.operator, repr(self.left), repr(self.right))
 
 class Add(BinOp):
     def __init__(self, left, right):
-        super().__init__('+', left, right)
+        super().__init__('+', left, right, 2)
 
 class Sub(BinOp):
     def __init__(self, left, right):
-        super().__init__('-', left, right)
+        super().__init__('-', left, right, 2)
 
 class Mul(BinOp):
     def __init__(self, left, right):
